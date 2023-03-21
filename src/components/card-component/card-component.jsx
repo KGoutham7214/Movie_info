8ded56b399adf37e5b6ff9cf26064a2a49b1cef1
KGoutham7214 <202011034@diu.iiitvadodara.ac.in>
2023-03-22 04:41:04 +0530

    final

--- src/components/card-component/card-component.jsx
+++ src/components/card-component/card-component.jsx
@@ -1,10 +1,13 @@
-import { Movie } from "@material-ui/icons";
+
 import React, {useState} from "react";
 import './card-components-styles.css'
 import { useGlobalContext } from "../../contexts/context";
 
 const Card = () => {
   const {movie} = useGlobalContext();
+ 
+
+  console.log(movie.imdbID + "sdcs");
   return (
     <div className="movie-card">
     {
@@ -12,7 +15,7 @@ const Card = () => {
         return(
           <div className='' key={movie.imdbID}>
           
-          <div className="card-content">
+          <div className="card-content" onClick={()=>{location.href = `https://www.imdb.com/title/${movie.imdbID}/`}}>
           <img 
             key={movie.imdbID}
             alt={`movie ${movie.Title}`}
